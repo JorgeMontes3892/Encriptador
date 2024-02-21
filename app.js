ce766b05ce1c12f3260ac1efc5c93d788525eace
Jorge Lara <jorgemontes6049@outlook.com>
2024-02-20 19:12:09 -0600

    james and murcielago bug fixed

--- app.js
+++ app.js
@@ -1,48 +1,47 @@
 function validartexto(){
-    let texto = document.querySelector("#input-texto").value;
-    let validador = texto.match(/^[a-z]*$/);
+  let texto = document.querySelector("#input-texto").value;
+  let validador = texto.match(/^[a-z]*$/);
 
-    if (!validador || validador === 0){
-        alert("Solo son permitidos letras minusculas, sin acentos y sin espacios")
-        location.reload();
-        return true;
-    }
+  if (!validador || validador === 0){
+      alert("Solo son permitidos letras minusculas, sin acentos y sin espacios")
+      location.reload();
+      return true;
+  }
 }
 
 function palabrarepetida(){
-    let texto = document.querySelector("#input-texto").value;
-    let palabraRepetida = texto.match(/imes/gi)
+  let texto = document.querySelector("#input-texto").value;
+  let palabraRepetida = texto.match(/imes/gi)
 
-    if(!palabraRepetida || palabraRepetida ===0)
-    return true;
+  if(!palabraRepetida || palabraRepetida ===0)
+  return true;
 }
 
 function cifrar (){
-    if (!validartexto(true)){
-    let texto = document.querySelector("#input-texto").value;
-    const textoCifrado = texto.replaceAll(/e/gi, "enter").replaceAll(/i/gi, "imes").replaceAll(/a/gi, "ai").replaceAll(/o/gi, "ober").replaceAll(/u/gi, "ufat");
-    document.querySelector(".text-input-salida").value = textoCifrado;
-    document.querySelector("#input-texto").value;
-    }
+  if (!validartexto(true)){
+  let texto = document.querySelector("#input-texto").value;
+  let textoCifrado = texto.replaceAll(/e/gi, "enter").replaceAll(/i/gi, "imes").replaceAll(/a/gi, "ai").replaceAll(/o/gi, "ober").replaceAll(/u/gi, "ufat");
+  document.querySelector(".text-input-salida").value = textoCifrado;
+  document.querySelector("#input-texto").value;
+  }
 }
 
 let boton1 = document.querySelector("#btn-cifrar"); boton1.onclick = cifrar;
 
 function desencriptar (){
-    if (!validartexto(true)){
-    let texto = document.querySelector("#input-texto").value;
-    const textoDesencriptado = texto.replaceAll(/ufat/gi,"u").replaceAll(/ober/gi, "o").replaceAll(/enter/gi, "e").replaceAll( /imes/gi, "i").replaceAll(/ai/gi, "a") /*jaimenters = james*/
-    document.querySelector(".text-input-salida").value = textoDesencriptado;
-    document.querySelector("#input-texto").value;
-    }
- }
+  if (!validartexto(true)){
+  let texto = document.querySelector("#input-texto").value;
+  const textoDesencriptado = texto.replaceAll(/ufat/gi,"u").replaceAll(/ober/gi, "o").replaceAll(/enter/gi, "e").replaceAll(/ai/gi, "a").replaceAll(/imes/gi, "i")/*jaimenters = james*/
+  document.querySelector(".text-input-salida").value = textoDesencriptado;
+  document.querySelector("#input-texto").value;
+  }
+}
 let boton2 = document.querySelector("#btn-decifrar"); boton2.onclick = desencriptar;
 
-
- function copiar(){
-    let texto =  document.querySelector(".text-input-salida");
-    navigator.clipboard.writeText(texto.value);
-    }
+function copiar(){
+  let texto =  document.querySelector(".text-input-salida");
+  navigator.clipboard.writeText(texto.value);
+  }
 
 let boton3 = document.querySelector("#btn-copy"); 
-    boton3.onclick = copiar
+  boton3.onclick = copiar
